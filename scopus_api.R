25e62492e309d3776faeaaa7082295c6b1c20091
Carlos Carbajal Pina (student) <c.carbajal-pina.1@research.gla.ac.uk>
2019-12-04 20:16:51 +0000

    CODE: Completion of general query by author using Scopus API

--- scopus_api.R
+++ scopus_api.R
@@ -6,8 +6,8 @@ require(tidyr)
 require(curl)
 
 # Loading authors dataset
-authors <- read.csv(file = "0_data/IPDMC_2019_Authors.csv", header = TRUE, stringsAsFactors = FALSE)
-colnames(authors) <- c("id", "full.name.raw", "surname", "name", "affiliation.raw")
+authors <- read.csv(file = "1_data/IPDMC_2019_Authors.csv", header = TRUE, stringsAsFactors = FALSE)
+names(authors) <- c("local.id", "full.name", "surname", "name", "affiliation.raw")
 
 # ===== Cleaing authors names
 # Transliteration of names
@@ -16,30 +16,59 @@ authors$name.clean <- try(iconv(authors$name, "latin1", "ASCII//TRANSLIT"))
 # Trimming extra spaces
 authors$surname.clean <- trimws(x = authors$surname.clean)
 authors$name.clean <- trimws(x = authors$name.clean)
+# To lowercase
+authors$surname.clean <- tolower(x = authors$surname.clean)
+authors$name.clean <- tolower(x = authors$name.clean)
 # Removing puntuation signs
-index <- grep(pattern = "[[:punct:]]", x = authors$surname.clean)
-authors$surname.clean[index] <- gsub(pattern = "[[:punct:]]", replacement = "",
-                                     x = authors$surname.clean[index])
-index <- grep(pattern = "[[:punct:]]", x = authors$name.clean)
-authors$name.clean[index] <- gsub(pattern = "[[:punct:]]", replacement = "",
-                                  x = authors$name.clean[index])
+temp <- grep(pattern = "[[:punct:]]", x = authors$surname.clean)
+authors$surname.clean[temp] <- gsub(pattern = "[[:punct:]]", replacement = "",
+                                     x = authors$surname.clean[temp])
+temp <- grep(pattern = "[[:punct:]]", x = authors$name.clean)
+authors$name.clean[temp] <- gsub(pattern = "[[:punct:]]", replacement = "",
+                                  x = authors$name.clean[temp])
+names(authors) <- c("local.id", "full.name", "surname", "name", "affiliation.raw")
 
 # ===== Setting up API key
 set_api_key(api_key = "199a37378d4d5af0155ff632ada3652f")
-if (have_api_key()) {
-  print("API Key accepted")
-  res = author_df(last_name = "acur", first_name = "nuran", verbose = FALSE, general = FALSE)
-  names(res)
-  head(res[, c("title", "journal", "description")])
-  unique(res$au_id)
-  unique(as.character(res$affilname_1))
-  
-  all_dat = author_data(last_name = "acur", 
-                        first_name = "nuran", verbose = FALSE, general = TRUE)
-  res2 = all_dat$df
-  res2 = res2 %>% 
-    rename(journal = `prism:publicationName`,
-           title = `dc:title`,
-           description = `dc:description`)
-  head(res[, c("title", "journal", "description")])
+if (have_api_key()) { print("API Key accepted") }
+
+# Quering routine
+scopus.data <- list()
+missing.data <- NA
+for (i in 1:nrow(authors)) {
+  # Progress update
+  if (i%%as.integer(nrow(authors)/20) == 0) { 
+    print(paste(c(round(i/nrow(authors)*100,0), "% completed"), collapse = ""))
+  }
+  # Quering Scopus
+  temp <- try(author_df(last_name = authors$surname.clean[i], 
+                             first_name = authors$name.clean[i], 
+                             verbose = FALSE, general = TRUE),
+                   silent = TRUE)
+  # Skiping errors
+  if (class(temp) == "try-error") {
+    missing.data <- c(missing.data, i)
+    next
+  }
+  # Binding search to general dataset
+  temp$local.id <- i
+  scopus.data[[i]] <- temp
 }
+
+# ===== Storing information extracted =====
+# Binding authors data dataframe
+scopus.data.flat <- bind_rows(scopus.data)
+# Adding orginal data
+temp <- subset(x = authors, select = c("local.id", "full.name", "name.clean", "surname.clean"))
+scopus.data.flat <- merge(x = scopus.data.flat, y = temp, by = "local.id")
+
+# Storing data froma authors not found.
+not.found <- as.data.frame(as.integer(missing.data[-1]))
+names(not.found) <- "local.id"
+not.found <- merge(x = not.found, y = authors, by = "local.id")
+
+# Storing external files
+save(list = c("authors", "not.found", "scopus.data", "scopus.data.flat"),
+     file = "3_output/0_scopus_query.RData")
+write.csv(x = scopus.data.flat, file = "3_output/1_scopus_query.csv")
+write.csv(x = not.found, file = "3_output/2_authors_not_found.csv")
