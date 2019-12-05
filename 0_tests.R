b4b93e131d9b485420bb20cbf8e9063bd3398a51
Carlos Carbajal Pina (student) <c.carbajal-pina.1@research.gla.ac.uk>
2019-12-05 17:10:23 +0000

    Testing code file

---
+++ 0_tests.R
@@ -0,0 +1,17 @@
+# Loading required libraries
+require(dplyr)
+require(tidytext)
+
+#Loading 
+text <- c("Because I could not stop for Death -",
+          "He kindly stopped for me -",
+          "The Carriage held but just Ourselves -",
+          "and Immortality")
+
+
+text
+
+text_df <- tibble(line = 1:4, text = text)
+
+
+text_df
