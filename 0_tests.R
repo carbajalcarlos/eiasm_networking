ddfbadf2d7cc931ee7fe3b20ef4bf17100cb3a61
Carlos Carbajal Pina (student) <c.carbajal-pina.1@research.gla.ac.uk>
2019-12-05 18:19:49 +0000

    CODING: Test code

--- 0_tests.R
+++ 0_tests.R
@@ -1,17 +1,53 @@
 # Loading required libraries
-require(dplyr)
+require(topicmodels)
+
+data("AssociatedPress")
+class(AssociatedPress)
+
+# Creation of LDA model searching for two topics (k=2)
+ap_lda <- LDA(AssociatedPress, k = 2, control = list(seed = 359))
+ap_lda
+
+# Interpretation of the model
 require(tidytext)
+ap_topics <- tidy(ap_lda, matrix = "beta")
+ap_topics
 
-#Loading 
-text <- c("Because I could not stop for Death -",
-          "He kindly stopped for me -",
-          "The Carriage held but just Ourselves -",
-          "and Immortality")
+# Plotting resutls
+require(ggplot2)
+require(dplyr)
+
+ap_top_terms <- ap_topics %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta)
 
 
-text
+ap_top_terms %>%
+  mutate(term = reorder_within(term, beta, topic)) %>%
+  ggplot(aes(term, beta, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  coord_flip() +
+  scale_x_reordered()
 
-text_df <- tibble(line = 1:4, text = text)
+# Visualisation of topic contrast
+require(tidyr)
 
+beta_spread <- ap_topics %>%
+  mutate(topic = paste0("topic", topic)) %>%
+  spread(topic, beta) %>%
+  filter(topic1 > .001 | topic2 > .001) %>%
+  mutate(log_ratio = log2(topic2 / topic1))
 
-text_df
+beta_spread
+
+# topic probabilities
+ap_documents <- tidy(ap_lda, matrix = "gamma")
+ap_documents
+
+tidy(AssociatedPress) %>%
+  filter(document == 6) %>%
+  arrange(desc(count))
+
